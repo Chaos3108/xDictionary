9ce6f7ddf08d8b256af2cd6d7d8b9e5fc0227e3a
yuva kishore <yuvakishorekolli@gmail.com>
2025-03-16 23:03:23 +0530

    Added dictionary code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [defintion, setDefinition] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const dictionaryData = [
+    {
+      word: "React",
+      meaning: "A JavaScript library for building user interfaces.",
+    },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." },
+  ];
+  const handleForm = (e) => {
+    setIsDisabled(true);
+    e.preventDefault();
+    const meaning = dictionaryData.filter(
+      (item) => item?.word.toLowerCase() === word?.toLowerCase()
+    );
+    console.log(meaning, word);
+    if (meaning.length > 0) {
+      setDefinition(meaning[0]?.meaning);
+    } else {
+      setDefinition("Word not found in the dictionary");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Dictionary App</h1>
+      <form onSubmit={(e) => handleForm(e)}>
+        <input
+          value={word}
+          onChange={(e) => {
+            setWord(e.target.value);
+          }}
+          placeholder="Search for a word..."
+        ></input>
+        <button type="submit"> submit</button>
+      </form>
+      <h3>Definition:</h3> <p>{defintion}</p>
     </div>
   );
 }
